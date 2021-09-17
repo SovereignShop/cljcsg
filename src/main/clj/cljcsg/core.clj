120e3a757f4aa79dc5fc6ab67f9e56f513a17e02
John Collins <john.collins@braincorp.com>
2021-09-17 13:54:12 -0700

    Demo working

---
+++ src/main/clj/cljcsg/core.clj
@@ -0,0 +1,25 @@
+(ns main.clj.cljcsg.core
+  (:import
+   [eu.mihosoft.jcsg Cube Sphere CSG]
+   [eu.mihosoft.vvecmath Transform]
+   [java.nio.file Paths]
+   [eu.mihosoft.jcsg FileUtil]))
+
+(def cube (.toCSG (new Cube 2)))
+(def sphere (.toCSG (Sphere. 1.25 10 10)))
+
+(def cube-plus-sphere (.union cube sphere))
+(def cube-minus-sphere (.difference cube sphere))
+(def cube-intersect-sphere (.intersect cube sphere))
+
+(.transformed sphere (.translateX (Transform/unity) 3))
+
+(def union
+  (-> cube
+    (.union (.transformed sphere (.translateX (Transform/unity) 3)))
+    (.union (.transformed cube-plus-sphere (.translateX (Transform/unity) 6)))
+    (.union (.transformed cube-minus-sphere (.translateX (Transform/unity) 9)))
+    (.union (.transformed cube-intersect-sphere (.translateX (Transform/unity) 12)))))
+
+(FileUtil/write (Paths/get "sample.stl" (into-array String []))
+                (.toStlString union))
